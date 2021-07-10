7aea5bf6e83c0f2830f6b6d7cb9b3ca9297824dc
ClaySamuelC <44789280+ClaySamuelC@users.noreply.github.com>
2021-07-10 12:40:42 -0700

    Add Recursive Tile Revealing

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,23 @@
 
 struct tile
 {
-	uint8_t tile;
+	int neighbors;
 	bool revealed;
 	bool flagged;
 	bool mined;
 };
+
+void revealTile(tile& t)
+{
+	t.flagged = false;
+	t.revealed = true;
+}
+
+void flagTile(tile& t)
+{
+	if (!t.revealed)
+		t.flagged = !t.flagged;
+}
 
 class Minesweeper : public olc::PixelGameEngine
 {
@@ -25,6 +37,30 @@ public:
 	std::unique_ptr<tile[]> field;
 
 public:
+	int getNeighborMineCount(int i)
+	{
+		if (field[i].mined)
+			return 0;
+
+		int n = 0;
+
+		int xStart = (i % vFieldSize.x == 0) ? 0 : -1;
+		int xEnd = (i % vFieldSize.x == vFieldSize.x - 1) ? 0 : 1;
+		int yStart = (i < vFieldSize.x) ? 0 : -1;
+		int yEnd = (i >= vFieldSize.x * (vFieldSize.y - 1)) ? 0 : 1;
+		for (int x = xStart; x <= xEnd; x++)
+		{
+			for (int y = yStart; y <= yEnd; y++)
+			{
+				int l = i + (y * vFieldSize.x) + x;
+				if (l != i && field[l].mined)
+					n++;
+			}
+		}
+
+		return n;
+	}
+
 	void shuffleField()
 	{
 		for (int i = vFieldSize.x * vFieldSize.y - 1; i >= 0; i--)
@@ -33,6 +69,42 @@ public:
 			tile tmp = field[i];
 			field[i] = field[r];
 			field[r] = tmp;
+		}
+	}
+
+	void setNeighborCounts()
+	{
+		for (int i = 0; i < vFieldSize.x * vFieldSize.y; i++)
+		{
+			field[i].neighbors = getNeighborMineCount(i);
+		}
+	}
+
+	void resetField()
+	{
+		shuffleField();
+		setNeighborCounts();
+	}
+
+	void recursiveReveal(int i)
+	{
+		revealTile(field[i]);
+
+		if (field[i].neighbors == 0 && !field[i].mined)
+		{
+			int xStart = (i % vFieldSize.x == 0) ? 0 : -1;
+			int xEnd = (i % vFieldSize.x == vFieldSize.x - 1) ? 0 : 1;
+			int yStart = (i < vFieldSize.x) ? 0 : -1;
+			int yEnd = (i >= vFieldSize.x * (vFieldSize.y - 1)) ? 0 : 1;
+			for (int x = xStart; x <= xEnd; x++)
+			{
+				for (int y = yStart; y <= yEnd; y++)
+				{
+					int l = i + (y * vFieldSize.x) + x;
+					if (l != i && !field[l].revealed && !field[l].mined)
+						recursiveReveal(l);
+				}
+			}
 		}
 	}
 
@@ -47,7 +119,7 @@ public:
 			field[i] = { 0, false, false, (i < nMineTotal) ? true : false };
 		}
 
-		shuffleField();
+		resetField();
 
 		return true;
 	}
@@ -65,20 +137,18 @@ public:
 
 		Clear(olc::BLACK);
 
-
-		// Input:
+		// Input
 		if (GetKey(olc::SPACE).bPressed)
-			shuffleField();
+			resetField();
 
 		// Mouse input
 		olc::vi2d vMouse = { GetMouseX(), GetMouseY() };
 		olc::vi2d vTileSelected = vMouse / vTileSize;
 		
 		if (GetMouse(0).bPressed)
-		{
-			field[vTileSelected.y * vFieldSize.x + vTileSelected.x].flagged = 
-				!field[vTileSelected.y * vFieldSize.x + vTileSelected.x].flagged;
-		}
+			recursiveReveal(vTileSelected.y * vFieldSize.x + vTileSelected.x);
+		if (GetMouse(1).bPressed)
+			flagTile(field[vTileSelected.y * vFieldSize.x + vTileSelected.x]);
 
 		// Draw field
 		for (int y = 0; y < vFieldSize.y; y++)
@@ -88,12 +158,17 @@ public:
 				olc::Pixel color;
 				tile curr = field[y * vFieldSize.x + x];
 
-				if		(curr.flagged)	color = olc::GREEN;
-				else if (curr.mined)	color = olc::RED;
-				else if (curr.revealed) color = olc::WHITE;
-				else					color = olc::GREY;
+				if		(curr.flagged)
+					color = olc::GREEN;
+				else if (curr.revealed)
+					color = (curr.mined) ? olc::RED : olc::WHITE;
+				else
+					color = olc::GREY;
 
 				FillRect(x * vTileSize.x, y * vTileSize.y, vTileSize.x - 1, vTileSize.y - 1, color);
+
+				if (curr.revealed && curr.neighbors > 0)
+					DrawString(olc::vi2d(x * vTileSize.x + 1, y * vTileSize.y + 1), std::to_string(curr.neighbors), olc::GREEN);
 			}
 		}
 
@@ -105,7 +180,7 @@ public:
 int main()
 {
 	Minesweeper demo;
-	if (demo.Construct(demo.vFieldSize.x * demo.vTileSize.x, demo.vFieldSize.y * demo.vTileSize.y, 4, 4))
+	if (demo.Construct(demo.vFieldSize.x * demo.vTileSize.x, demo.vFieldSize.y * demo.vTileSize.y, 4, 4)) 
 		demo.Start();
 
 	return 0;
